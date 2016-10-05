7c82fb618872ed7901c01a0032cb4ba5f440d296
bizcuite <bizcuite@gmail.com>
2016-10-05 09:07:43 +0200

    smdk-common adapt for cm14

--- common.mk
+++ common.mk
@@ -20,10 +20,10 @@ DEVICE_PACKAGE_OVERLAYS := $(COMMON_PATH)/overlay
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # The gps config appropriate for this device
-$(call inherit-product, device/common/gps/gps_us_supl.mk)
+#$(call inherit-product, device/common/gps/gps_us_supl.mk)
 
 # Init files
 PRODUCT_COPY_FILES := \
@@ -68,7 +68,6 @@ PRODUCT_COPY_FILES += \
 
 # Packages
 PRODUCT_PACKAGES += \
-    AdvancedDisplay \
     audio.a2dp.default \
     audio.primary.smdk4x12 \
     audio.r_submix.default \
@@ -86,7 +85,11 @@ PRODUCT_PACKAGES += \
     libUMP \
     lights.exynos4 \
     macloader \
-    tinymix
+    tinymix \
+    AudioFX 
+
+#to keep
+#AdvancedDisplay
 
 # Stlport    
 PRODUCT_PACKAGES += \
